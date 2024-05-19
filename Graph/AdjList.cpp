d6396e42b71429a472b974e52a605de172f3830b
Himanshu Rai <1si23ad016@sit.ac.in>
2024-05-19 23:23:01 +0530

    "Graph Implementation"

---
+++ Graph/AdjList.cpp
@@ -0,0 +1,132 @@
+#include<iostream>
+#include <vector>
+using namespace std;
+
+
+/*
+// Adjacency List
+//Undirected Unwerighted Graph
+
+int main(){
+
+    int vertex, edge;
+    cout<<"Enter the number of vertices and edges: ";
+    cin>>vertex>>edge;
+
+    vector<vector<int>> AdjList(vertex);
+
+    int u,v;
+    cout<<"Enter the edges: ";
+    for(int i=0;i<edge;i++){
+        cin>>u>>v;
+        AdjList[u].push_back(v);
+        AdjList[v].push_back(u);
+    }
+    
+    cout<<"Adjacency List: "<<endl;
+    for(int i=0;i<vertex;i++){
+        cout<<i<<" -> ";
+        for(int j=0;j<AdjList[i].size();j++){
+            cout<<AdjList[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+}
+*/
+/*
+
+// Adjacency List
+//Undirected weighted Graph
+
+int main(){
+
+    int vertex, edge;
+    cout<<"Enter the number of vertices and edges: ";
+    cin>>vertex>>edge;
+
+    vector<vector<pair<int,int>>> AdjList(vertex);
+
+    int u,v,weight;
+    cout<<"Enter the edges: ";
+    for(int i=0;i<edge;i++){
+        cin>>u>>v>>weight;
+        AdjList[u].push_back(make_pair(v,weight));
+        AdjList[v].push_back(make_pair(u,weight));
+    }
+    
+    cout<<"Adjacency List: "<<endl;
+    for(int i=0;i<vertex;i++){
+        cout<<i<<" -> ";
+        for(int j=0;j<AdjList[i].size();j++){
+            cout<<AdjList[i][j].first<<" "<<AdjList[i][j].second<<" ";
+        }
+        cout<<endl;
+    }
+
+}
+*/
+
+
+/*
+// Adjacency List
+//Directed Unweighted Graph
+
+int main(){
+    
+        int vertex, edge;
+        cout<<"Enter the number of vertices and edges: ";
+        cin>>vertex>>edge;
+    
+        vector<vector<int>> AdjList(vertex);
+    
+        int u,v;
+        cout<<"Enter the edges: ";
+        for(int i=0;i<edge;i++){
+            cin>>u>>v;
+            AdjList[u].push_back(v);
+        }
+        
+        cout<<"Adjacency List: "<<endl;
+        for(int i=0;i<vertex;i++){
+            cout<<i<<" -> ";
+            for(int j=0;j<AdjList[i].size();j++){
+                cout<<AdjList[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+    
+    }
+
+*/
+
+
+
+// Adjacency List
+//Directed weighted Graph
+
+int main(){
+    
+        int vertex, edge;
+        cout<<"Enter the number of vertices and edges: ";
+        cin>>vertex>>edge;
+    
+        vector<vector<pair<int,int>>> AdjList(vertex);
+    
+        int u,v,weight;
+        cout<<"Enter the edges: ";
+        for(int i=0;i<edge;i++){
+            cin>>u>>v>>weight;
+            AdjList[u].push_back(make_pair(v,weight));
+        }
+        
+        cout<<"Adjacency List: "<<endl;
+        for(int i=0;i<vertex;i++){
+            cout<<i<<" -> ";
+            for(int j=0;j<AdjList[i].size();j++){
+                cout<<AdjList[i][j].first<<" "<<AdjList[i][j].second<<" ";
+            }
+            cout<<endl;
+        }
+    
+    }
---
+++ Graph/AdjMat.cpp
@@ -0,0 +1,131 @@
+#include<iostream>
+#include <vector>
+
+using namespace std;
+
+/*
+// Adjacency Matrix
+//Undirected Unwerighted Graph
+
+int main(){
+
+    int vertex, edge;
+    cout<<"Enter the number of vertices and edges: ";
+    cin>>vertex>>edge;
+
+    vector<vector<bool>> AdjMat(vertex,vector<bool>(vertex,0));
+
+    int u,v;
+    cout<<"Enter the edges: ";
+    for(int i=0;i<edge;i++){
+        cin>>u>>v;
+        AdjMat[u][v]=1;
+        AdjMat[v][u]=1;
+    }
+    
+    cout<<"Adjacency Matrix: "<<endl;
+    for(int i=0;i<vertex;i++){
+        for(int j=0;j<vertex;j++){
+            cout<<AdjMat[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+}
+*/
+
+
+/*
+// Adjacency Matrix
+//Undirected Weighted Graph
+
+int main(){
+
+    int vertex, edge;
+    cout<<"Enter the number of vertices and edges: ";
+    cin>>vertex>>edge;
+
+    vector<vector<int>> AdjMat(vertex,vector<int>(vertex,0)); //take int value if weighted
+
+    int u,v,weight;
+    cout<<"Enter the edges: ";
+    for(int i=0;i<edge;i++){
+        cin>>u>>v>>weight;
+        AdjMat[u][v]=weight;
+        AdjMat[v][u]=weight;
+    }
+    
+    cout<<"Adjacency Matrix: "<<endl;
+    for(int i=0;i<vertex;i++){
+        for(int j=0;j<vertex;j++){
+            cout<<AdjMat[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+}
+
+*/
+
+/*
+
+// Adjacency Matrix
+//directed Graph
+
+int main(){
+
+    int vertex, edge;
+    cout<<"Enter the number of vertices and edges: ";
+    cin>>vertex>>edge;
+
+    vector<vector<bool>> AdjMat(vertex,vector<bool>(vertex,0));
+
+    int u,v;
+    cout<<"Enter the edges: ";
+    for(int i=0;i<edge;i++){
+        cin>>u>>v;
+        AdjMat[u][v]=1;
+        
+    }
+    
+    cout<<"Adjacency Matrix: "<<endl;
+    for(int i=0;i<vertex;i++){
+        for(int j=0;j<vertex;j++){
+            cout<<AdjMat[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+}
+*/
+
+
+
+//Adjacency Matrix
+//directed Weighted Graph
+
+int main(){
+
+    int vertex, edge;
+    cout<<"Enter the number of vertices and edges: ";
+    cin>>vertex>>edge;
+
+    vector<vector<int>> AdjMat(vertex,vector<int>(vertex,0));
+
+    int u,v,weight;
+    cout<<"Enter the edges: ";
+    for(int i=0;i<edge;i++){
+        cin>>u>>v>>weight;
+        AdjMat[u][v]=weight;
+        
+    }
+    
+    cout<<"Adjacency Matrix: "<<endl;
+    for(int i=0;i<vertex;i++){
+        for(int j=0;j<vertex;j++){
+            cout<<AdjMat[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+}
